d408954cf08b983ecefa64255172c91a53ef991f
Mohamed Kardosha <mohamedkrdsh9@gmail.com>
2026-02-20 21:29:54 +0200

    fix: setup files and verify_tests file

--- 05-merge-conflict/setup.sh
+++ 05-merge-conflict/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 rm -rf .git *.txt
 git init -q
+DEFAULT_BRANCH=$(git symbolic-ref --short HEAD)
 echo 'TODO list:' > todo.txt
 git add . && git commit -q -m 'Initial'
 git checkout -q -b feature-conflict
 echo 'Feature Update' >> todo.txt
 git add . && git commit -q -m 'Feature Update'
-git checkout -q main
+git checkout -q "$DEFAULT_BRANCH"
 echo 'Main Update' >> todo.txt
 git add . && git commit -q -m 'Main Update'
 echo 'Exercise 05 initialized. Task: Resolve conflict in todo.txt.'
--- 06-stash/setup.sh
+++ 06-stash/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 rm -rf .git *.json *.txt
 git init -q
+DEFAULT_BRANCH=$(git symbolic-ref --short HEAD)
 echo '{"version": "1.0"}' > config.json
 git add .
 git commit -q -m "Initial config"
 git checkout -q -b urgent-fix
 echo '{"version": "1.1-beta"}' > config.json
 git commit -q -a -m "Update version on fix branch"
-git checkout -q main
+git checkout -q "$DEFAULT_BRANCH"
 echo '{"version": "BROKEN-WORK-IN-PROGRESS"}' > config.json 
 echo "Exercise 06 initialized."
-echo "You have uncommitted changes on 'main'. You need to switch to 'urgent-fix'."
+echo "You have uncommitted changes on '$DEFAULT_BRANCH'. You need to switch to 'urgent-fix'."
